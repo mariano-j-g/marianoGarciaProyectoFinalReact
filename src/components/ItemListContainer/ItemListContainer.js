f9e711cffee5cc9f39e783fc72c92cecaf8499d0
Miguel Javier Dev <mariano812811@gmail.com>
2023-09-29 19:46:03 -0300

    cambio titulo

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -6,33 +6,37 @@ import { getDocs, collection, query, where } from "firebase/firestore";
 import { db } from "../../services/config";
 
 const ItemListContainer = () => {
-    const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
 
-    const { categoryId } = useParams()
+  const { categoryId } = useParams();
 
-    useEffect(() => {
-        const collectionRef = categoryId
-            ? query(collection(db, "productos"), where("categoria", "==", categoryId))
-            : collection(db, "productos")
+  useEffect(() => {
+    const collectionRef = categoryId
+      ? query(collection(db, "productos"), where("categoria", "==", categoryId))
+      : collection(db, "productos");
 
-        getDocs(collectionRef)
-            .then(response => {
-                const productsAdapted = response.docs.map(doc => {
-                    const data = doc.data()
-                    return { id: doc.id, ...data }
-                })
-                setProducts(productsAdapted)
-            })
-            .catch(error => console.log(error))
-    }, [categoryId])
+    getDocs(collectionRef)
+      .then((response) => {
+        const productsAdapted = response.docs.map((doc) => {
+          const data = doc.data();
+          return { id: doc.id, ...data };
+        });
+        setProducts(productsAdapted);
+      })
+      .catch((error) => console.log(error));
+  }, [categoryId]);
 
-    return (
-        <div>
-            <h1>Harry Potter Shop</h1>
-            <h4>El hechizo invocador de serpientes, <span className="fst-italic">Serpensortia</span>, es un hechizo de transformación que conjura una serpiente del extremo de la varita.</h4>
-            <ItemList products={products} />
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Harry Potter Shop</h1>
+      <h4>
+        El hechizo invocador de serpientes,{" "}
+        <span className="fst-italic">buzon</span>, es un hechizo de
+        transformación que conjura una serpiente del extremo de la varita.
+      </h4>
+      <ItemList products={products} />
+    </div>
+  );
+};
 
 export default ItemListContainer;
